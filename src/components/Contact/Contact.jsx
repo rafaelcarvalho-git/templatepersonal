b28a01eb019e626fc8ca7d2619b8cc07d4f37ef5
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-01-04 10:35:59 -0300

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,29 +4,33 @@ import Fade from 'react-reveal/Fade'
 
 function Contact() {
     return (
-        <section className="text-center py-2 my-5 bg-contact">
-            <Fade>
-                <div className="container my-5">
-                    <div className="row align-items-center">
+        <section className="text-center py-2 my-5">
+            <div className="container my-5">
+                <div className="row align-items-center">
+                    <Fade left>
                         <a href="tel:+88988573004" className="contact-item-hover col-lg-4 mb-5 mb-lg-0 text-decoration-none">
                             <BsTelephone className="contact-icon mb-3" />
                             <h3>(88) 98812-3456</h3>
                             <p>Seg a sab 8h - 18h</p>
                         </a>
+                    </Fade>
+                    <Fade up>
                         <a href="https://goo.gl/maps/H8iavTc2CnMzzvma7" target="_blank" rel="noreferrer noopener" className="contact-item-hover col-lg-4 mb-5 mb-lg-0 text-decoration-none">
                             <BsGeoAlt className="contact-icon mb-3" />
                             <h3>Juazeiro do Norte</h3>
                             <p>Ceará, Brasil</p>
                         </a>
+                    </Fade>
+                    <Fade right>
                         <a href="mailto:rafaskyplay@gmail.com" className="contact-item-hover col-lg-4 mb-5 mb-lg-0 text-decoration-none">
                             <BsEnvelope className="contact-icon mb-3" />
                             <h3 className="text-lowercase">personal@gmail.com</h3>
                             <p>Aguardo a sua mensagem</p>
                         </a>
-                    </div>
+                    </Fade>
                 </div>
-            </Fade>
-        </section>
+            </div>
+        </section >
     );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,12 @@ import { BsWhatsapp, BsInstagram, BsEnvelope, BsFacebook } from "react-icons/bs"
 function Header() {
     return (
         <header>
-            <div className="container">
-                <Zoom>
+            <Zoom>
+                <div className="container">
                     <div className="header-text text-center mt-4">
                         <h5>Olá, eu sou</h5>
-                        <h1 className="header-under mx-auto">RAFAEL CARVALHO</h1>
-                        <h2 className="header-subtitle mt-3 mb-5"><Typed strings={[
+                        <h1 className="header-under mx-auto text-shadow">RAFAEL CARVALHO</h1>
+                        <h2 className="header-subtitle mt-3 mb-5 text-shadow"><Typed strings={[
                             'Personal Trainer',
                             'Lutador Profissional',
                             'Atleta',
@@ -19,7 +19,7 @@ function Header() {
                             typeSpeed={50}
                             backSpeed={60} loop />
                         </h2>
-                        <div className="header-icons mx-auto d-flex mb-5">
+                        <div className="header-icons mx-auto d-flex mb-5 text-shadow">
                             <a target="_blank" rel="noreferrer noopener" href="https://github.com/rafaelcarvalho-git" className="header-icon mx-auto"><BsWhatsapp /></a>
                             <a target="_blank" rel="noreferrer noopener" href="https://api.whatsapp.com/send/?phone=5588988573004&text&app_absent=0" className="header-icon mx-auto"><BsInstagram /></a>
                             <a target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/rafaelcarvalho-ti/" className="header-icon mx-auto"><BsEnvelope /></a>
@@ -27,8 +27,8 @@ function Header() {
                         </div>
                         <a href="#" className="btn-1 btn-float">Agende sua aula experimental</a>
                     </div>
-                </Zoom>
-            </div>
+                </div>
+            </Zoom>
         </header>
     );
 }
