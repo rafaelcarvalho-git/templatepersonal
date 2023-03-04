91390f7cdceae9067f9203b5f9f16cdb5d7bc69c
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-03-03 23:31:06 -0300

    implementing styled components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,32 @@
 import * as S from './styles'
-import './Header.css'
 import Zoom from 'react-reveal/Zoom'
+import { BsWhatsapp, BsInstagram, BsEnvelope, BsFacebook } from 'react-icons/bs'
 import Typed from 'react-typed'
-import { BsWhatsapp, BsInstagram, BsEnvelope, BsFacebook } from "react-icons/bs"
-import { Container } from '../../global'
+import logo from '../../assets/img/logo.png'
+import { Button } from '../../global'
 
 function Header() {
     return (
         <S.Header>
             <Zoom>
-                <Container>
-                    <S.HeaderText className="header-text text-center mt-4">
-                        <h5>Olá, eu sou</h5>
-                        <h1 className="header-under mx-auto text-shadow">RAFAEL CARVALHO</h1>
-                        <h2 className="header-subtitle mt-3 mb-5 text-shadow"><Typed strings={[
+                <S.HeaderContainer>
+                    <img src={logo} alt='personal trainer' />
+                    <S.HeaderTyped>
+                        <Typed strings={[
                             'Personal Trainer',
                             'Lutador Profissional',
                             'Atleta',
                             'Dançarino']}
-                            typeSpeed={50}
-                            backSpeed={60} loop />
-                        </h2>
-                        <div className="header-icons mx-auto d-flex mb-5 text-shadow">
-                            <a target="_blank" rel="noreferrer noopener" href="https://github.com/rafaelcarvalho-git" className="header-icon mx-auto"><BsWhatsapp /></a>
-                            <a target="_blank" rel="noreferrer noopener" href="https://api.whatsapp.com/send/?phone=5588988573004&text&app_absent=0" className="header-icon mx-auto"><BsInstagram /></a>
-                            <a target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/rafaelcarvalho-ti/" className="header-icon mx-auto"><BsEnvelope /></a>
-                            <a target="_blank" rel="noreferrer noopener" href="mailto:rafaskyplay@gmail.com" className="header-icon mx-auto"><BsFacebook /></a>
-                        </div>
-                        <a href="#" className="btn-1 btn-float">Agende sua aula experimental</a>
-                    </S.HeaderText>
-                </Container>
+                            typeSpeed={50} backSpeed={60} loop />
+                    </S.HeaderTyped>
+                    <S.HeaderIcons>
+                        <S.HeaderIcon target='_blank' rel='noreferrer noopener' href='https://github.com/rafaelcarvalho-git'><BsWhatsapp /></S.HeaderIcon>
+                        <S.HeaderIcon target='_blank' rel='noreferrer noopener' href='https://api.whatsapp.com/send/?phone=5588988573004&text&app_absent=0'><BsInstagram /></S.HeaderIcon>
+                        <S.HeaderIcon target='_blank' rel='noreferrer noopener' href='https://www.linkedin.com/in/rafaelcarvalho-ti/'><BsEnvelope /></S.HeaderIcon>
+                        <S.HeaderIcon target='_blank' rel='noreferrer noopener' href='mailto:rafaskyplay@gmail.com'><BsFacebook /></S.HeaderIcon>
+                    </S.HeaderIcons>
+                    <Button href='#'>Agende sua aula experimental</Button>
+                </S.HeaderContainer>
             </Zoom>
         </S.Header>
     );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,31 +1,64 @@
 import styled from 'styled-components'
-import { primaryColor, quaternaryColor } from '../../global'
+import { Container, primaryColor, quaternaryColor, secondaryColor } from '../../global'
 import headerImg from '../../assets/img/header-bg.png'
 
 export const Header = styled.header`
     height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: center;
     background-color: #8EC5FC;
     background-image: linear-gradient(62deg, #a5d1fdc9 20%, #716accda 100%);
     background: url(${headerImg});
     background-position: center;
     background-attachment: fixed;
-    background-size: cover;
+    background-size: cover;    
 `;
 
-
-export const HeaderText = styled.div`
-   
+export const HeaderContainer = styled(Container)` 
+    text-align: center;
 `;
 
+export const HeaderTyped = styled.h2`
+    text-shadow: 0 0 1em #252525, 0 0 1em #252525;
+    margin: 1.5rem auto 2.5rem auto;
+    color: white;
+    font-family: Poppins, sans-serif;
+    font-size: 28px;
+    text-shadow: 0 0 0.1em #252525, 0 0 0.2em #252525;
+    
+    @media (max-width: 527px) {
+        font-size: 32px;
+    }
 
+    @media (max-width: 394px) {
+        font-size: 30px;
+    }
 
+    @media (max-width: 361px) {
+        font-size: 29.5px;
+    }
+`;
 
 export const HeaderIcons = styled.div`
-   
+    max-width: 280px;
+    display: flex;
+    justify-content: space-around;
+    margin: auto auto 3rem auto;
+    text-shadow: 0 0 1em #252525, 0 0 1em #252525;
 `;
 
+export const HeaderIcon = styled.a`
+    font-size: 32px;
+    color: white;
+    transition: all 0.2s ease 0s;
+    text-shadow: 0 0 0.2em black, 0 0 0.2em ${secondaryColor} !important;
 
+    &:hover {
+        color: ${secondaryColor};
+    }
 
-
+    @media (max-width: 527px) {
+        font-size: 30px !important;      
+    }
+`;
